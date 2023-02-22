de76574334f5da9d26a31cff5683e2f2d6bf3cb4
Burak Erdilli <berdilli19@gmail.com>
2023-02-22 23:49:48 +0300

    all files added

---
+++ contact.php
@@ -0,0 +1,21 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    
+    $email_form = 'Styles Restaurant Site';
+    $email_subject = 'New Message From Styles Contact';
+    $email_body = "Name: $name.\n".
+                  "Email: $email.\n".
+                  "Message: $message.\n";
+    
+    $to ="berdilli19@gmail.com";
+    $headers = "From: $email_from \r\n";
+    $headers .= "Reply-To: $email \r\n";
+    mail($to,$email_subject,$email_body,$headers);
+    
+    header("location: index.html")
+        
+
+
+?>
